9359cf387b8f65c72128081e6af6883e845794d5
Naldo <naldorck@gmail.com>
2021-03-04 23:29:13 -0500

    base

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,18 +2,14 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin') 
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
-const TerserPlugin = require('terser-webpack-plugin')
-const { CleanWebpackPlugin  } = require('clean-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/'
+    filename: 'bundle.js'
   },
-  mode: 'production',
+  mode: 'development',
   resolve: {
     extensions: ['.js', '.jsx'],
     alias: {
@@ -61,14 +57,11 @@ module.exports = {
     }),
     new MiniCssExtractPlugin({
       filename: '[name].css'
-    }),
-    new CleanWebpackPlugin(),
+    })
   ],
-  optimization: {
-    minimize: true, 
-    minimizer: [
-      new CssMinimizerPlugin(),
-      new TerserPlugin()
-    ]
-  }
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 3005
+  },
 }
